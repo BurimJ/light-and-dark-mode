4a043608258f858a9a55860d378b1400a4e68de1
BurimJ <ipyroburnz@gmail.com>
2023-03-15 10:30:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 "use strict";
 
+// console.log(5 > 4);
+// console.log("apple" > "pineapple");
+// "2" > "12";
+// console.log(undefined == null);
+// console.log(undefined === null);
+// console.log(null == "\n0\n");
+// console.log(null === +"\n0\n");
+// console.log("race" === "petl");
+// console.log("race" !== "petl");
+
 window.addEventListener("load", initApp);
 
 function initApp() {
-    //event listener for select on HTML
-    document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
+  //event listener for select on HTML
+  document
+    .querySelector("#select-color-mode")
+    .addEventListener("change", modeSelected);
 }
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
 function modeSelected() {
-    console.log("New color mode selected");
+  console.log("New color mode selected");
 }
