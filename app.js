9a359f945f0f865da8d2ad461d48e3fa2d51b20c
BurimJ <ipyroburnz@gmail.com>
2023-03-15 11:38:38 +0100

    n

--- app.js
+++ app.js
@@ -17,9 +17,39 @@ function initApp() {
   document
     .querySelector("#select-color-mode")
     .addEventListener("change", modeSelected);
+
+  detectUserPreference();
 }
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
 function modeSelected() {
-  console.log("New color mode selected");
+  const selectedColorMode = this.value;
+  console.log("SelectColorMode");
+
+  changeMode(selectedColorMode);
+  savedUserColorMode(selectedColorMode);
+}
+
+function changeMode(mode) {
+  resetColor();
+
+  if (mode === "dark") {
+    document.body.classList.add("dark-mode");
+  } else if (mode === "green") {
+    document.body.classList.add("green-mode");
+  } else if (mode === "gold") {
+    document.body.classList.add("gold-mode");
+  }
+}
+
+function resetColor() {
+  document.body.classList.remove("dark-mode");
+  document.body.classList.remove("green-mode");
+  document.body.classList.remove("gold-mode");
+}
+
+function detectUserPreference() {}
+function readUserColorMode() {}
+function savedUserColorMode() {
+  localStorage.setItem("userColorMode", mode);
 }
